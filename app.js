cdae2006d818067ff7a0a924f024d7e5b58e7cc0
Yuval Dekel <thedekel@gmail.com>
2013-01-27 11:48:15 -0500

    slide 6

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-//simple web server in express
+//simple GET and POST server
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('hello world');
+app.get('*', function(req, res){ //the "*" indicates that any path will respond with this function
+  res.send('Thank you for GETting my page.'); //this is where we respond with a simple message
+});
+
+app.post('*', function(req, res){ //identical to the above example, but for POST requests
+  res.send('Thank you for POSTting my page.'); //this is where we respond with a simple message
 });
 
 app.listen(8080);
