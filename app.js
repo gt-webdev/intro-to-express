38c1c2abf17fc2a85e28fbb685f174b109007ed0
Yuval Dekel <thedekel@gmail.com>
2013-01-27 12:10:37 -0500

    slide 10

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-//simple GET and POST server
+//GET a simple list
 var express = require('express');
 var app = express();
 
-app.get('*', function(req, res){ //the "*" indicates that any path will respond with this function
-  res.send('Thank you for GETting my page.'); //this is where we respond with a simple message
+var myList = ["stop", "hammer", "time"]; //we instantiate an empty list.
+//notice that the list is not instantiated within a function so it's global
+
+app.get('*', function(req, res){ 
+  //now we want to display the list 
+  var responseString = "Currently, the list have:", i;
+  for (i=0; i<myList.length; i+=1){
+    responseString += "\n" + myList[i];
+  }
+  res.send(responseString); 
+  //the crude and simple way
+  //res.send(myList); 
 });
 
 app.post('*', function(req, res){ //identical to the above example, but for POST requests
